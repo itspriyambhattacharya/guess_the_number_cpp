c743c396e030bc0701efcdd172c849356a2fd9aa
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2024-10-15 19:15:42 +0530

    added code to enter choice from user

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 int main(void)
 {
-
+    int ch; // To store choice of easy, medium or hard
+    cout << "\nGuess the Number\n";
+    cout << "\nEnter level:\n1 for Easy (Number between 1 to 10)\n2 for Medium (Number between 1 to 100)\n3 for Hard (Number between -500 to 500)\nEnter your choice:\t";
+    cin >> ch;
     return 0;
 }
